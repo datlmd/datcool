0fd88914bd8df84701e83659dc42e92017e6c11f
Dat Le <z.dat.leminh@gree.net>
2018-12-28 15:19:47 +0700

    init

---
+++ application/views/templates_c/82c3360e6f1aa29cfda4e4bb9c7a1b4485a04df7_0.file.header.tpl.php
@@ -0,0 +1,56 @@
+<?php
+/* Smarty version 3.1.33, created on 2018-12-28 08:43:31
+  from '/Applications/XAMPP/xamppfiles/htdocs/dev/datcool/application/views/templates/header.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5c25d4239f73a9_18100600',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '82c3360e6f1aa29cfda4e4bb9c7a1b4485a04df7' => 
+    array (
+      0 => '/Applications/XAMPP/xamppfiles/htdocs/dev/datcool/application/views/templates/header.tpl',
+      1 => 1464082202,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5c25d4239f73a9_18100600 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<!--[if ie 6]><![endif]-->
+
+<head>
+	<meta charset="utf-8" />
+	<title><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+ - <?php echo $_smarty_tpl->tpl_vars['name']->value;?>
+</title> 
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Mono:400,400italic' rel='stylesheet' type='text/css'>
+
+	<style type="text/css">
+	
+		body {background-color: #fff; color: #000; width: 800px; font-family: 'Roboto Mono', 'courier', 'courier new', monotype;}
+		h1, h2 {background-color: #fff; color: #999; font-family: 'Roboto Slab', 'Times New Roman', Georgia, serif;}
+		h1 {font-size: 2em;}
+		h2 {font-size: 1.5em;}
+		em {border: solid #000 1px; padding: 0 5px; font-style: normal;}
+		label {display: inline-block; width: 10em; text-align: right;}
+		input[type="submit"] {margin: 1em 0 0 10em;}
+		.error {background-color: #ff0; color: #c00;}
+		.message {background-color: #fff; color: #0c0;}
+	
+	</style>
+</head> 
+<body>
+<?php }
+}
---
+++ application/views/templates_c/f366e6b6186e3c11a46d03c06bb8f9f7433a9e39_0.file.footer.tpl.php
@@ -0,0 +1,27 @@
+<?php
+/* Smarty version 3.1.33, created on 2018-12-28 08:44:36
+  from '/Applications/XAMPP/xamppfiles/htdocs/dev/datcool/application/views/templates/footer.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5c25d4644d0cb8_54865505',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'f366e6b6186e3c11a46d03c06bb8f9f7433a9e39' => 
+    array (
+      0 => '/Applications/XAMPP/xamppfiles/htdocs/dev/datcool/application/views/templates/footer.tpl',
+      1 => 1254128842,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5c25d4644d0cb8_54865505 (Smarty_Internal_Template $_smarty_tpl) {
+?></body>
+</html>
+<?php }
+}
